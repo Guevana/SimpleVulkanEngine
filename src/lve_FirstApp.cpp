dc1071a7724132fa4137b4bfb37296bce5d2d351
Guevana <2100929226@qq.com>
2026-09-16 16:46:13 +0800

    Added support for global unified buffers and descriptorSets, and refactored the rendering system to use the new buffer classes

--- src/lve_FirstApp.cpp
+++ src/lve_FirstApp.cpp
@@ -21,12 +21,43 @@
 #include <array>
 
 namespace lve {
-    FirstAPP::FirstAPP() {loadGameObjects();}
+    FirstAPP::FirstAPP() {
+        globalPool = LveDescriptorPool::Builder(lveDevice)
+                        .setMaxSets(LveSwapChain::MAX_FRAMES_IN_FLIGHT)
+                        .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, LveSwapChain::MAX_FRAMES_IN_FLIGHT)
+                        .build();
+        loadGameObjects();
+    }
 
     FirstAPP::~FirstAPP() {}
 
     void FirstAPP::run() {
-        SimpleRenderSystem simpleRenderSystem{lveDevice, lveRenderer.getSwapChainRenderPass()};
+        std::vector<std::unique_ptr<LveBuffer>> uboBuffers(LveSwapChain::MAX_FRAMES_IN_FLIGHT);
+        for (int i= 0; i < uboBuffers.size(); i++) {
+            uboBuffers[i] = std::make_unique<LveBuffer>(
+            lveDevice,
+            sizeof(GlobalUbo),
+            1,
+            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+
+            uboBuffers[i]->map();
+        }
+        auto globalSetLayout = LveDescriptorSetLayout::Builder(lveDevice)
+                                    .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT)
+                                    .build();
+
+        std::vector<VkDescriptorSet> globalDescriptorSets(LveSwapChain::MAX_FRAMES_IN_FLIGHT);
+        for (int i = 0; i < globalDescriptorSets.size(); i++) {
+            auto bufferInfo = uboBuffers[i]->descriptorInfo();
+            LveDescriptorWriter(*globalSetLayout, *globalPool)
+                .writeBuffer(0, &bufferInfo)
+                .build(globalDescriptorSets[i]);
+        }
+
+
+
+        SimpleRenderSystem simpleRenderSystem{lveDevice, lveRenderer.getSwapChainRenderPass(), globalSetLayout->getDescriptorSetLayout()};
         LveImguiLayer imguiLayer{lveWindow, lveDevice, lveRenderer.getSwapChainInfo()};
         DebugUI debugUI{};
         LveCamera camera{};
@@ -38,18 +69,30 @@ namespace lve {
         auto captureRevision = lveWindow.captureRevision();
         
 
-        while (!lveWindow.shouldClose())
-        {
+        while (!lveWindow.shouldClose()) {
             glfwPollEvents();
-
-            auto commandBuffer = lveRenderer.beginFrame();
-            if (!commandBuffer) continue;
-            imguiLayer.syncSwapChain(lveRenderer.getSwapChainInfo());
-            imguiLayer.beginFrame();
 
             auto newTime = std::chrono::high_resolution_clock::now();
             float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
             currentTime = newTime;
+
+            /* ==== 以下为渲染循环 ====*/
+            auto commandBuffer = lveRenderer.beginFrame();
+            if (!commandBuffer) continue;
+
+            //update
+            int frameIndex = lveRenderer.getFrameIndex();
+
+            FrameInfo frameInfo {
+                frameIndex,
+                frameTime,
+                commandBuffer,
+                camera,
+                globalDescriptorSets[frameIndex]
+            };
+
+            imguiLayer.syncSwapChain(lveRenderer.getSwapChainInfo());
+            imguiLayer.beginFrame();
 
             debugUI.draw(gameObjects, viewerObject, cameraController, lveRenderer, frameTime);
             const auto& io = ImGui::GetIO();
@@ -64,19 +107,23 @@ namespace lve {
             
             float aspect = lveRenderer.getAspectRadio();
 
-            //正交投影
-            //camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
-
             camera.setPerspectiveProjection(glm::radians(debugUI.fieldOfView()), aspect, 0.1f, 10.0f);
 
-            {
-                lveRenderer.beginSwapChainRenderPass(commandBuffer);
-                simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
-                imguiLayer.render(commandBuffer);
-                lveRenderer.endSwapChainRenderPass(commandBuffer);
-                lveRenderer.endFrame();
-            }
+            GlobalUbo ubo{};
+            ubo.projectionView = camera.getProjection() * camera.getView();
+            uboBuffers[frameIndex]->writeToBuffer(&ubo);
+            uboBuffers[frameIndex]->flush();
 
+            //render
+            lveRenderer.beginSwapChainRenderPass(commandBuffer);
+            simpleRenderSystem.renderGameObjects(frameInfo, gameObjects);
+            imguiLayer.render(commandBuffer);
+            lveRenderer.endSwapChainRenderPass(commandBuffer);
+
+
+            lveRenderer.endFrame();
+
+            /*==== 结束渲染循环 ====*/
         }
         
         vkDeviceWaitIdle(lveDevice.device());
--- src/lve_FirstApp.hpp
+++ src/lve_FirstApp.hpp
@@ -4,13 +4,21 @@
 #include "lve_device.hpp"
 #include "lve_GameObject.hpp"
 #include "lve_renderer.hpp"
+#include "lve_buffer.hpp"
+#include "lve_descriptors.hpp"
 
 #include <memory>
 #include <vector>
 
 namespace lve{
-   
-
+    /**
+     * 统一缓冲区数据结构
+     */
+    struct GlobalUbo {
+        glm::mat4 projectionView{1.0f};
+        glm::vec3 lightDirection = glm::normalize(glm::vec3{1.0f, -3.0f, -1.0f});
+    };
+    
     class FirstAPP{
         public:
 
@@ -33,6 +41,7 @@ namespace lve{
         LveDevice lveDevice{lveWindow};
         LveRenderer lveRenderer{lveWindow, lveDevice};
         
+        std::unique_ptr<LveDescriptorPool> globalPool{};
         std::vector<LveGameObject> gameObjects;
     };
 
